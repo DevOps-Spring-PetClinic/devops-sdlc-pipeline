8089fc111a14dc86a03c69c36fba17b952d8e0b0
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-04-28 15:13:28 +0100

    changes

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -100,7 +100,6 @@ public class Owner extends Person {
 
 	/**
 	 * Return the Pet with the given name, or null if none found for this Owner.
-	 * 
 	 * @param name to test
 	 * @return a pet if pet name is already in use
 	 */
@@ -110,7 +109,6 @@ public class Owner extends Person {
 
 	/**
 	 * Return the Pet with the given id, or null if none found for this Owner.
-	 * 
 	 * @param id to test
 	 * @return a pet if pet id is already in use
 	 */
@@ -128,7 +126,6 @@ public class Owner extends Person {
 
 	/**
 	 * Return the Pet with the given name, or null if none found for this Owner.
-	 * 
 	 * @param name to test
 	 * @return a pet if pet name is already in use
 	 */
@@ -148,18 +145,17 @@ public class Owner extends Person {
 	@Override
 	public String toString() {
 		return new ToStringCreator(this).append("id", this.getId())
-				.append("new", this.isNew())
-				.append("lastName", this.getLastName())
-				.append("firstName", this.getFirstName())
-				.append("address", this.address)
-				.append("city", this.city)
-				.append("telephone", this.telephone)
-				.toString();
+			.append("new", this.isNew())
+			.append("lastName", this.getLastName())
+			.append("firstName", this.getFirstName())
+			.append("address", this.address)
+			.append("city", this.city)
+			.append("telephone", this.telephone)
+			.toString();
 	}
 
 	/**
 	 * Adds the given {@link Visit} to the {@link Pet} with the given identifier.
-	 * 
 	 * @param petId the identifier of the {@link Pet}, must not be {@literal null}.
 	 * @param visit the visit to add, must not be {@literal null}.
 	 */
